cbb27f3144b1f9349989f20450bed7767182a80a
Sunyu Kim <mauveine.kim@gmail.com>
2026-04-18 14:53:10 +0100

    dop_2604.js

--- data/dop.js
+++ data/dop.js
@@ -19,7 +19,7 @@
 var defined_dop_projects = [
 
   {
-    youtube:  "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    youtube:  "https://youtu.be/qgGrbOM78Mg?si=q2Sys5EyB8931tLc",
     category: "Short Film",
     title:    "Unseen Frames",
     position: "Director of Photography",
@@ -28,19 +28,35 @@ var defined_dop_projects = [
   },
 
   {
-    youtube:  "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    category: "Documentary",
-    title:    "Hollow Light",
-    position: "Director of Photography",
-    credits:  "Directed by Clara Voss"
+    youtube:  "https://www.instagram.com/p/DCjAwwPobUj/",
+    category: "Vertical Short-Form Series",
+    title:    "I Cook",
+    position: "Director of Photography & Supervising Editor",
+    credits:  "Produced by Sunyu Kim, Directed by Heather Jarek"
   },
 
   {
-    youtube:  "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    category: "Music Video",
-    title:    "Between Stations",
+    youtube:  "https://youtu.be/BSrgDQrSE0s?si=onwEPyoDvhTue9fj",
+    category: "Short Film",
+    title:    "The Void",
     position: "Director of Photography",
-    credits:  "Artist: Juno Park"
-  }
+    credits:  "Produced and Directed by Eliza Salnikova"
+  },
 
+   {
+    youtube:  "www.behance.net/gallery/197468023/MarginaliaAlice",
+    category: "3D Animation",
+    title:    "MarginAlice",
+    position: "Director of Photography (Live-Action)",
+    credits:  "Produced by Yolanda Zhang, Tiffany Otoyo, and Vallerie Toumahu"
+  },
+  
+  {
+    youtube:  "https://youtu.be/CuuKFYdM7F8?si=e0v_fHnwZq9oV-lZ",
+    category: "Short Documentary",
+    title:    "Dizziak Advertisement",
+    position: "Director of Photography",
+    credits:  "Produced by Marco Capelli, et al., Directed by Pratham Bhatia"
+  },
+  
 ];
