5480bf10c9af9ec75d4730e1c63ed07da025e6a8
Sunyu Kim <mauveine.kim@gmail.com>
2026-04-18 15:07:33 +0100

    photos_2604_3.js

--- data/photos.js
+++ data/photos.js
@@ -26,6 +26,8 @@ var defined_photos = [
   "18-016 Large.jpeg",
   "18-020 Large.jpeg",
   "18-023 Large.jpeg",
-  "18-026 Large.jpeg"
+  "18-026 Large.jpeg",
+  "18-027 Large.jpeg",
+  "18-044 Large.jpeg"
 
 ];
