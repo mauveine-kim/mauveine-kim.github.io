3f076f95ed785a793895ddf78456268700510e03
Sunyu Kim <mauveine.kim@gmail.com>
2026-04-22 15:22:03 +0100

    Update editing.js

--- data/editing.js
+++ data/editing.js
@@ -43,6 +43,14 @@ var defined_editing_projects = [
     position:  "Supervising Editor & Director of Photography",
     credits:   "Produced by Sunyu Kim, Directed by Heather Jarek and Eliza Salnikova"
   },
+
+  {
+    link:      "https://youtu.be/19qQ3qHKrDU?si=0PH7bsf5kWeyUl-A",
+    category:  "Review",
+    title:     "영화같은 동영상을 위한 도구 - 모먼트(Moment) 애너모픽 렌즈 리뷰",
+    position:  "Editor & Director of Photography",
+    credits:   "Produced, Written, and Directed by Sunyu Kim",
+  },
   
   {
     link:      "https://youtu.be/q6qsEIt9rYg?si=OWqCJeLcy5PpmqkC&t=5",
